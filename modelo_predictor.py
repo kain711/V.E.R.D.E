c89e7e0cb0fa217bb31b9c63335648556a755bf9
kain711 <andres66689@mail.com>
2025-07-09 19:30:13 -0500

    corregir modelo predictor

--- modelo_predictor.py
+++ modelo_predictor.py
@@ -34,21 +34,22 @@ import tensorflow as tf
 # Cargar el modelo una sola vez
 modelo = tf.keras.models.load_model("modelo_multietiqueta.h5")
 
-# Clases en el orden exacto del entrenamiento
-clases = ['Acchocha', 'Altamizo', 'Torongil', 'cedron', 'pushasha', 'romero', 'ruda', 'sabila', 'tomate']
+# Nombres de las etiquetas en el mismo orden que se entrenó el modelo
+etiquetas = ['comestible', 'medicinal']
 
-def predecir_multietiqueta(imagen, umbral=0.5):
+def predecir_etiquetas(imagen, umbral=0.5):
     try:
         imagen = imagen.convert("RGB")
         imagen = imagen.resize((100, 100))
         img_array = np.array(imagen) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
 
-        predicciones = modelo.predict(img_array)[0]  # vector de prob. para cada clase
-        clases_predichas = [clase for clase, prob in zip(clases, predicciones) if prob >= umbral]
-        resultado_detallado = {clase: float(prob) for clase, prob in zip(clases, predicciones)}
+        # Predicción
+        predicciones = modelo.predict(img_array)[0]  # vector de tamaño 2
+        resultado = {etiqueta: float(prob) for etiqueta, prob in zip(etiquetas, predicciones)}
+        etiquetas_activadas = [etiqueta for etiqueta, prob in resultado.items() if prob >= umbral]
 
-        return clases_predichas, resultado_detallado
+        return etiquetas_activadas, resultado
     except Exception as e:
         print(f"Error al procesar la imagen: {e}")
         return None, None
