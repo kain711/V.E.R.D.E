393eab10bd62412ca3ffa2444946a4348b03d7eb
kain711 <andres66689@mail.com>
2025-07-09 20:03:50 -0500

    modificar orden de las clases

--- modelo_predictor.py
+++ modelo_predictor.py
@@ -1,32 +1,4 @@
-"""
-import tensorflow as tf
-import numpy as np
-from PIL import Image
 
-# Cargar el modelo una sola vez
-modelo = tf.keras.models.load_model("modelo_multietiqueta.h5")
-
-# Clases en el orden exacto del entrenamiento
-clases = ['Acchocha','Altamizo', 'Torongil', 'cedron', 'pushasha', 'romero', 'ruda', 'sabila', 'tomate'] 
-
-def predecir(imagen):
-    try:
-        imagen = imagen.convert("RGB")
-        # Asegurar tamaño correcto
-        imagen = imagen.resize((100, 100))
-        img_array = np.array(imagen) / 255.0
-        img_array = np.expand_dims(img_array, axis=0)
-    
-        # Predicción
-        pred = modelo.predict(img_array)
-        clase_predicha = clases[np.argmax(pred)]
-        confianza = np.max(pred)
-
-        return clase_predicha, confianza
-    except Exception as e:
-        print(f"Error al procesar la imagen: {e}")
-        return None, None
-"""
 import numpy as np
 from PIL import Image
 import tensorflow as tf
@@ -35,7 +7,7 @@ import tensorflow as tf
 modelo = tf.keras.models.load_model("modelo_multietiqueta.h5")
 
 # Nombres de las etiquetas en el mismo orden que se entrenó el modelo
-etiquetas = ['comestible', 'medicinal']
+etiquetas = ['medicinal', 'comestible']
 
 def predecir_etiquetas(imagen, umbral=0.5):
     try:
