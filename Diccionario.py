b6b2d54525ceedf1084e941e14c7932328e00385
kain711 <andres66689@mail.com>
2025-07-15 07:08:10 -0500

    arreglar t tipo de dato en temperatura

--- Diccionario.py
+++ Diccionario.py
@@ -27,7 +27,8 @@ def mostrar_formulario_planta():
         st.markdown("##### Condiciones ambientales donde prospera la planta")
         #numero float
         
-        temperatura=st.number_input("Temperatura °C",format="%.2f",min_value=0,max_value=30,step=1)
+        temperatura=st.number_input("Temperatura °C",format="%.2f",min_value=0.0,max_value=30.0,step=1)
+        
         ph_suelo=st.number_input("PH del suelo",format="%.2f",min_value=1,max_value=14,step=0.1)
         tipo_suelo=st.selectbox("Tipo de suelo",["Arcilloso","","Arenoso","Loamoso","Otro(especifique)"])
         nivel_humedad=st.number_input("Nivel de humedad",format="%.2f",min_value=0,max_value=100,step=1)
